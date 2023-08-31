fb8df242dd0cd4cad5daaf7140315a678574b1f7
thobyza <thobyy.athaya@gmail.com>
2023-08-31 10:40:44 +0700

    steps

---
+++ gitExercise.js
@@ -0,0 +1,15 @@
+/* STEPS
+    cara menyambungkan local ke git
+    1. git init
+    2. git remote add origin ...        {url from gitHub}
+    3. git remote -v                    {cek remote version}
+    
+    cara push ke gitHub
+    1. git add . {add ke staging}
+    2. git commit -m "[your message]"   {add message}
+    3. git push origin [your branch]
+
+    cara pindah branch
+    
+
+*/
