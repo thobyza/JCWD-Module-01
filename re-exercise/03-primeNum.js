d8d46c563c567b8aeeb5aa7d8ee0ba3e88f6e91f
thobyza <thobyy.athaya@gmail.com>
2023-09-02 13:17:50 +0700

    session-02 update

--- re-exercise/03-primeNum.js
+++ re-exercise/03-primeNum.js
@@ -1,13 +1,38 @@
 // [3] Write a code to check whether the number is prime number or not
-let num = 57;
-let divider = 0;
 
-for (let i = 0; i <= 10; i++) {
-    if (num % i == 0) {
-        divider++;
-        // console.log(divider);
-    }
-}
+// let num = 57;
+// let divider = 0;
 
-console.log(`${num} ${divider === 2 ? 'is' : 'is not'} a prime number`);
+// for (let i = 0; i <= 10; i++) {
+//     if (num % i == 0) {
+//         divider++;
+//         // console.log(divider);
+//     }
+// }
 
+// console.log(`${num} ${divider === 2 ? 'is' : 'is not'} a prime number`);
+
+// ^1 sept
+// [3] Write a code to check whether the number is prime number or not
+/*
+prime number -> divider = 2 > 0, the number itself
+*/
+
+// let isPrime = 24;
+// let divider = 0;
+
+// for (let i = 1; i <= 10; i++) {
+
+//     if (isPrime % i == 0) {
+//         divider = divider + 1;
+//         // console.log(divider);
+//     }
+// }
+
+// if (divider == 2) {
+//     console.log(`${isPrime} is a prime number`);
+// } else if (divider > 2) {
+//     console.log(`${isPrime} is not a prime number`);
+// }
+
+// 
--- re-exercise/04-sumOf.js
+++ re-exercise/04-sumOf.js
@@ -12,4 +12,23 @@ for (let i = 2; i <= num; i++) {
     console.log(desc);
 }
 
-console.log(`${num} → ${desc} = ${result}`);
+// console.log(`${num} → ${desc} = ${result}`);
+
+
+// ^2 sept
+// Write a code to find the sum of the numbers 1 to N.
+// Example : 5 → 1 + 2 + 3 + 4 + 5 = 15
+
+// let inputNum = 5;
+// let penampung = "1";
+// let result = 1;
+
+// for (let i = 2; i <= inputNum; i++) {
+
+//     penampung = `${penampung} + ${i}`;
+//     console.log(penampung);
+
+//     result = (result + i);
+// }
+
+// console.log(`${inputNum} → ${penampung} = ${result}`);
