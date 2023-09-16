efc1a2184e9965770994e70bc9d8440968604dd5
thobyza <thobyy.athaya@gmail.com>
2023-09-16 18:33:07 +0700

    update session-12

--- 01-majority.js
+++ 01-majority.js
@@ -24,3 +24,30 @@ function majorityElement(arr) {
 }
 
 console.log(majorityElement([2, 4, 2, 1, 4, 1, 4, 1, 2, 2, 4, 4]));
+// 
+
+
+// Write a code to find most common character from a string
+let str = "Dizzy izzy";
+
+function commonChar(s) {
+    let candidateMajority = "";
+    let count = 1;
+
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === candidateMajority) {
+            count++
+        } else {
+            count--;
+        }
+
+        if (count === 0) {
+            candidateMajority = s[i]
+            count = 1;
+        }
+    }
+
+    return candidateMajority
+}
+
+console.log(commonChar(str));
---
+++ 04-commonStr.js
@@ -0,0 +1,9 @@
+// [4] Write a code to find most common character from a string
+
+let str = "Dizzy Izzy";
+
+function commonStr(s) {
+
+}
+
+console.log(commonStr(str));
