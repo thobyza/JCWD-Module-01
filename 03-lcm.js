f8e37ebc209bef1ce953b8ec13e0a3f7541d28e5
thobyza <thobyy.athaya@gmail.com>
2023-09-19 07:09:26 +0700

    session-15

--- 03-lcm.js
+++ 03-lcm.js
@@ -1,17 +1,17 @@
 // Write a code to find LCM of two numbers → use do … while.
 
-function findLcm(num1, num2) {
-    let count = 0;
-    let lcm = [];
+// function findLcm(num1, num2) {
+//     let count = 0;
+//     let lcm = [];
 
-    do {
-        if (num1 % count == 0 && num2 % count == 0) {
-            lcm.push(count);
-        }
-        count++;
-    } while (count <= num1 && count <= num2);
+//     do {
+//         if (num1 % count == 0 && num2 % count == 0) {
+//             lcm.push(count);
+//         }
+//         count++;
+//     } while (count <= num1 && count <= num2);
 
-    return lcm[1];
-}
+//     return lcm;
+// }
 
-console.log(findLcm(17,));
+// console.log(findLcm(24, 30));
